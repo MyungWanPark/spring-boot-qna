ecf41765f8b4bdff2bd177beba895eb6eb779233
MyungWanPark <mywanpark@gmail.com>
2021-03-08 17:03:08 +0900

    Add: add user info by using Arraylist

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -1,13 +1,20 @@
 package com.codessquad.qna.controller;
 
+import com.codessquad.qna.user.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class UserController {
+    private List<User> users = new ArrayList<>();
+
     @PostMapping("user/create")
-    public String create(String userId, String password){
-        System.out.println("userId: "+userId);
+    public String create(User user){
+        users.add(user);
+        System.out.println("User : "+user);
         return "welcome";
 
     }
