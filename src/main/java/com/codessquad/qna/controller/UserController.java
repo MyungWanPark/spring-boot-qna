f549a1d922dcadfa04f135539e972d50622068ec
MyungWanPark <mywanpark@gmail.com>
2021-03-08 15:47:37 +0900

    feat: create User.java & make getter and setter method

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -1,4 +1,14 @@
 package com.codessquad.qna.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class UserController {
+    @PostMapping("user/create")
+    public String create(String userId, String password){
+        System.out.println("userId: "+userId);
+        return "welcome";
+
+    }
 }
